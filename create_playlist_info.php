c55e53dc8046a233e3b951983e7f968c015692b3
freakpants <freakpants@gmail.com>
2017-09-28 16:21:29 +0200

    fixed bug when calling getAlbum

--- create_playlist_info.php
+++ create_playlist_info.php
@@ -59,9 +59,9 @@ function create_playlist_info(){
 			$sth->execute();
 			$results = $sth->fetch();
 			
-			$album_art = get_album_art(utf8_encode($results['artistName']),utf8_encode($results['collectionName']),$results['collectionId']);
+			$info = get_album_art(utf8_encode($results['artistName']),utf8_encode($results['collectionName']),$results['collectionId']);
 			
-			array_push( $playlist_object->album_art, $album_art);
+			array_push( $playlist_object->album_art, $info['album_art']);
 			
 		}
 		
