22e884cc7e98497e79ab534c9ccce5bf03ec0d88
freakpants <freakpants@gmail.com>
2021-08-26 14:10:49 +0200

    displaying details of single playlist

---
+++ php/playlist.php
@@ -0,0 +1,26 @@
+<?php
+require("connect.php");
+
+global $dbh;
+$dbh = new PDO('mysql:host=localhost;dbname=' . $dbname , $user, $password);
+
+$sql = "SET NAMES 'utf-8'";
+$dbh->query($sql);
+
+$sql = "SELECT `id`,`name`,`public`,`description` FROM playlists WHERE `id` = :id";
+$sth = $dbh->prepare($sql);
+$sth->execute(array(":id" => $_GET['id']));
+$results = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+$playlist = $results;
+
+$sql = "SELECT service, playlist_id, track_id as id, relation_id, itunes_tracks.artistName, itunes_tracks.previewUrl, itunes_tracks.collectionName FROM songs_in_playlist LEFT JOIN itunes_tracks ON songs_in_playlist.track_id = itunes_tracks.id  WHERE `playlist_id` = :id";
+$sth = $dbh->prepare($sql);
+$sth->execute(array(":id" => $_GET['id']));
+$results = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+$playlist['tracks'] = $results;
+
+echo(json_encode($playlist));
+
+?>
--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
 import axios from "axios";
 import "./App.css";
 import React from "react";
-import { DataGrid } from "@material-ui/data-grid";
+import { DataGrid, useGridApiRef } from "@material-ui/data-grid";
 
 /* 
 Command to copy php scripts
 cp -rf ~/musicguess-data/php/* /mnt/c/gamerbased/htdocs/musicguess-data/ */
 
-/* const columns = [
-  { field: "id", headerName: "ID", width: 90 },
-  {
-    field: "name",
-    headerName: "First name",
-    width: 150,
-    editable: true,
-  },
-  {
-    field: "public",
-    headerName: "Last name",
-    width: 150,
-    editable: true,
-  },
-  {
-    field: "description",
-    headerName: "Age",
-    type: "number",
-    width: 110,
-    editable: true,
-  }
-]; */
 
 const columns = [
   { field: 'id', headerName: 'ID' },
@@ -52,6 +30,12 @@ const columns = [
   }
 ];
 
+const tracks= [
+  { field: 'service', headerName: 'service', width: 130 },
+  { field: 'artistName', headerName: 'Artist', width: 200 },
+  { field: 'collectionName', headerName: 'Album', width: 200 },
+];
+
 
 
 class App extends React.Component {
@@ -61,9 +45,16 @@ class App extends React.Component {
     super(props);
     // this.get_playlists = this.get_playlists.bind(this);
     // this.get_playlists();
-    this.state = {playlists: [
-      { id: 1, name: 'Snow', public: 'Jon', description: "something" }
-    ]}; 
+    this.state = {
+      playlists: [{ id: 1, name: 'Snow', public: 'Jon', description: "something" } ], 
+      columns: columns,
+      playlist: [{ id: 0, service: "itunessss"}],
+      playlistColumns: tracks,
+      location: "Playlist Overview"
+   }; 
+
+  
+    this.displaySinglePlaylist = this.displaySinglePlaylist.bind(this); 
 
     axios
     .post("http://localhost/musicguess-data/playlists.php")
@@ -84,22 +75,50 @@ class App extends React.Component {
       console.log(error);
       // manipulate the error response here
     });
+
+
+
   }
+
+  displaySinglePlaylist(e){
+    let id = e.id;
+    this.setState({location: "Playlist " + id});
+    axios
+    .post("http://localhost/musicguess-data/playlist.php?id=" + id )
+    .then((response) => {
+      // manipulate the response here
+      // this.setState({playlist: response.data.tracks});
+      this.setState({playlists: response.data.tracks, columns: tracks, location: response.data[0].name });
+      console.log(response.data);
+      /* const parsed = JSON.parse(response.data);
+    if (parsed.success) {
+      alert('Anfrage erfolgreich abgeschickt');
+    } else {
+      alert('Fehler beim Absenden der Anfrage');
+      console.log(parsed.error);
+    } */
+    })
+    .catch(function (error) {
+      alert("Fehler beim Absenden der Anfrage");
+      console.log(error);
+      // manipulate the error response here
+    });
+  }
+
   render() {
 
 
     return (
       <div>
-        Playlist Editor
+        <h1>{this.state.location}</h1>
         <div style={{ height: "100vh", width: "100%" }}>
-          <DataGrid
+           <DataGrid
             rows={this.state.playlists}
-            columns={columns}
+            columns={this.state.columns}
             pageSize={20}
             rowsPerPageOptions={[5]}
-            checkboxSelection
-            disableSelectionOnClick
-          />
+            onRowClick={this.displaySinglePlaylist}
+          />  
         </div>
       </div>
     );
