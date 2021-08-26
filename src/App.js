0fe8743582f447e79375bb6e433c550b5727d1b7
freakpants <freakpants@gmail.com>
2021-08-26 14:27:00 +0200

    separate list for playlist details

--- src/App.js
+++ src/App.js
@@ -7,36 +7,34 @@ import { DataGrid, useGridApiRef } from "@material-ui/data-grid";
 Command to copy php scripts
 cp -rf ~/musicguess-data/php/* /mnt/c/gamerbased/htdocs/musicguess-data/ */
 
-
 const columns = [
-  { field: 'id', headerName: 'ID' },
+  { field: "id", headerName: "ID" },
   {
-    field: 'name',
-    headerName: 'Name',
+    field: "name",
+    headerName: "Name",
     width: 200,
     editable: true,
   },
   {
-    field: 'public',
-    headerName: 'Public',
+    field: "public",
+    headerName: "Public",
     width: 120,
     editable: true,
   },
   {
-    field: 'description',
-    headerName: 'Description',
+    field: "description",
+    headerName: "Description",
     width: 500,
     editable: true,
-  }
+  },
 ];
 
-const tracks= [
-  { field: 'service', headerName: 'service', width: 130 },
-  { field: 'artistName', headerName: 'Artist', width: 200 },
-  { field: 'collectionName', headerName: 'Album', width: 200 },
+const tracks = [
+  { field: "service", headerName: "service", width: 130, editable: true },
+  { field: "artistName", headerName: "Artist", width: 200, editable: true },
+  { field: "trackName", headerName: "Title", width: 200, editable: true },
+  { field: "collectionName", headerName: "Album", width: 200, editable: true },
 ];
-
-
 
 class App extends React.Component {
   playlists;
@@ -46,79 +44,98 @@ class App extends React.Component {
     // this.get_playlists = this.get_playlists.bind(this);
     // this.get_playlists();
     this.state = {
-      playlists: [{ id: 1, name: 'Snow', public: 'Jon', description: "something" } ], 
+      playlists: [
+        { id: 1, name: "Snow", public: "Jon", description: "something" },
+      ],
       columns: columns,
-      playlist: [{ id: 0, service: "itunessss"}],
-      playlistColumns: tracks,
-      location: "Playlist Overview"
-   }; 
+      playlist: [{ id: 0, service: "itunessss" }],
+      location: "overview",
+      locationTitle: "Playlist Overview",
+      selectedPlaylist: 0,
+    };
 
-  
-    this.displaySinglePlaylist = this.displaySinglePlaylist.bind(this); 
+    this.goToSelectedPlaylist = this.goToSelectedPlaylist.bind(this);
+    this.selectPlaylist = this.selectPlaylist.bind(this);
 
     axios
-    .post("http://localhost/musicguess-data/playlists.php")
-    .then((response) => {
-      // manipulate the response here
-      this.setState({playlists: response.data});
-      console.log(response.data);
-      /* const parsed = JSON.parse(response.data);
+      .post("http://localhost/musicguess-data/playlists.php")
+      .then((response) => {
+        // manipulate the response here
+        this.setState({ playlists: response.data });
+        console.log(response.data);
+        /* const parsed = JSON.parse(response.data);
     if (parsed.success) {
       alert('Anfrage erfolgreich abgeschickt');
     } else {
       alert('Fehler beim Absenden der Anfrage');
       console.log(parsed.error);
     } */
-    })
-    .catch(function (error) {
-      alert("Fehler beim Absenden der Anfrage");
-      console.log(error);
-      // manipulate the error response here
-    });
-
-
-
+      })
+      .catch(function (error) {
+        alert("Fehler beim Absenden der Anfrage");
+        console.log(error);
+        // manipulate the error response here
+      });
   }
 
-  displaySinglePlaylist(e){
+  selectPlaylist(e) {
     let id = e.id;
-    this.setState({location: "Playlist " + id});
+    this.setState({ selectedPlaylist: id });
+  }
+
+  goToSelectedPlaylist(e) {
     axios
-    .post("http://localhost/musicguess-data/playlist.php?id=" + id )
-    .then((response) => {
-      // manipulate the response here
-      // this.setState({playlist: response.data.tracks});
-      this.setState({playlists: response.data.tracks, columns: tracks, location: response.data[0].name });
-      console.log(response.data);
-      /* const parsed = JSON.parse(response.data);
-    if (parsed.success) {
-      alert('Anfrage erfolgreich abgeschickt');
-    } else {
-      alert('Fehler beim Absenden der Anfrage');
-      console.log(parsed.error);
-    } */
-    })
-    .catch(function (error) {
-      alert("Fehler beim Absenden der Anfrage");
-      console.log(error);
-      // manipulate the error response here
-    });
+      .post(
+        "http://localhost/musicguess-data/playlist.php?id=" +
+          this.state.selectedPlaylist
+      )
+      .then((response) => {
+        // manipulate the response here
+        // this.setState({playlist: response.data.tracks});
+        this.setState({
+          playlist: response.data.tracks,
+          columns: tracks,
+          locationTitle: response.data[0].name,
+          location: "singlePlaylist",
+        });
+        console.log(response.data);
+      });
   }
 
   render() {
-
-
     return (
       <div>
-        <h1>{this.state.location}</h1>
-        <div style={{ height: "100vh", width: "100%" }}>
-           <DataGrid
-            rows={this.state.playlists}
-            columns={this.state.columns}
-            pageSize={20}
-            rowsPerPageOptions={[5]}
-            onRowClick={this.displaySinglePlaylist}
-          />  
+        <h1>{this.state.locationTitle}</h1>
+        <div style={{ height: "85vh", width: "100%" }}>
+          {this.state.location === "overview" && (
+            <React.Fragment>
+              <DataGrid
+                rows={this.state.playlists}
+                columns={this.state.columns}
+                pageSize={100}
+                rowsPerPageOptions={[5]}
+                onRowClick={this.selectPlaylist}
+              />{" "}
+              <button onClick={this.goToSelectedPlaylist}>
+                Go to selected Playlist
+              </button>
+            </React.Fragment>
+          )}
+
+          {this.state.location === "singlePlaylist" && (
+            <React.Fragment>
+              <DataGrid
+                rows={this.state.playlist}
+                columns={tracks}
+                pageSize={100}
+                rowsPerPageOptions={[5]}
+              />
+                <button onClick={(e) => this.setState({location: "overview"})}>
+                Go back to Overview
+              </button>
+            </React.Fragment>
+
+          )}
         </div>
       </div>
     );
