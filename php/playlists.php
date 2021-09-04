e0f023d111d292271e9829856fc4b0bfacf22cd7
freakpants <freakpants@gmail.com>
2021-09-05 01:03:34 +0200

    added playlist count, small fix

--- php/playlists.php
+++ php/playlists.php
@@ -7,7 +7,7 @@ $dbh = new PDO('mysql:host=localhost;dbname=' . $dbname , $user, $password);
 $sql = "SET NAMES 'utf-8'";
 $dbh->query($sql);
 
-$sql = "SELECT `id`,`name`,`public`,`description` FROM playlists";
+$sql = "SELECT COUNT(songs_in_playlist.relation_id) AS track_count, `id`,`name`,`public`,`description` FROM playlists LEFT JOIN songs_in_playlist ON songs_in_playlist.playlist_id = playlists.id GROUP BY playlists.id";
 $sth = $dbh->prepare($sql);
 $sth->execute();
 $results = $sth->fetchAll(PDO::FETCH_ASSOC);
