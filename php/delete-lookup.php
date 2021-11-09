e707ec542fb00cd04ddcc9778cb6370f497c8627
freakpants <freakpants@gmail.com>
2021-11-09 16:54:54 +0100

    fixed id not getting from get

--- php/delete-lookup.php
+++ php/delete-lookup.php
@@ -6,7 +6,7 @@ $dbh = new PDO('mysql:host=localhost;dbname=' . $dbname , $user, $password);
 
 $sql = "DELETE FROM lookup WHERE `id` = :id";
 $sth = $dbh->prepare($sql);
-$sth->execute(array(":id" => $_POST['id'] ));
+$sth->execute(array(":id" => $_GET['id'] ));
 
 $count = $sth->rowCount();
 print("Deleted $count rows.\n");
