d375fc26ab87b7060e8385623cef8e34f79e925f
freakpants <freakpants@gmail.com>
2021-11-22 11:04:02 +0100

    use itunes live cover

--- php/get_wildcard.php
+++ php/get_wildcard.php
@@ -8,8 +8,8 @@ global $dbh;
 $dbh = new PDO('mysql:host=localhost;dbname=' . $dbname , $user, $password);
 
 
-$sql = "SELECT 'itunes' as service, id, artistName as artist, trackName as title, previewUrl as preview_url, collectionName as album, collectionViewUrl as buy_link,  collectionId
- FROM itunes_tracks WHERE 1 ORDER BY RAND() LIMIT 10";
+$sql = "SELECT 'itunes' as service, id, artistName as artist, trackName as title, previewUrl as preview_url, collectionName as album, collectionViewUrl as buy_link, collectionId, artworkUrl100
+ FROM itunes_tracks WHERE 1 ORDER BY RAND() LIMIT 100";
 
 $sth = $dbh->prepare($sql);
 $sth->execute();
@@ -17,7 +17,7 @@ $sth->execute();
 $wildcards = $sth->fetchAll(PDO::FETCH_ASSOC);
 foreach($wildcards as $key => $wildcard){
     $new_wild = $wildcard;
-    $new_wild['album_art'] = 'album_art/' . $wildcard['collectionId'] . '.jpg';
+    $new_wild['album_art'] =  preg_replace("/100x100bb\.jpg/","1000x1000bb.jpg",$wildcard['artworkUrl100']);
     $wildcards[$key] = $new_wild;
 }
 
