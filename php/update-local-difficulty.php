3b138b1512e50b468b4f09e8269b65a1edc3ec6c
freakpants <freakpants@gmail.com>
2022-05-19 17:14:26 +0200

    calculate correctpercent, averagetime, and mirror that down to local db

---
+++ php/update-local-difficulty.php
@@ -0,0 +1,73 @@
+<?php
+/* called by cron once a minute, updates 10 tracks */
+require("connect.php");
+
+global $dbh;
+$dbh = new PDO('mysql:host=localhost;dbname=' . $dbname , $user, $password);
+
+global $dbh_online;
+$dbh_online = new PDO('mysql:host=freakpants.ch;dbname=' . $dbname_online , $user_online, $password_online);
+
+// get all online tracks that have difficulty data
+$sql = "SELECT * FROM itunes_tracks WHERE correctCount > 0 OR inCorrectCount > 0";
+
+
+$sth = $dbh_online->prepare($sql);
+$sth->execute();
+$results = $sth->fetchAll(PDO::FETCH_ASSOC);
+// go over the results in a for each loop
+
+foreach($results as $track){
+    // echo the id
+    echo $track['id'] . '<br>';
+    echo 'Correct: ' . $track['correctCount'] . '<br>';
+    echo 'Incorrect: ' . $track['incorrectCount'] . '<br>';
+
+    // calculate average time
+    $correctTime = $track['correctTime'];
+    $correctCount  = $track['correctCount'];
+
+    if($correctCount > 0){
+        $averageTime = $correctTime / $correctCount;
+    } else {
+        $averageTime = 0;
+    }
+
+    // calculate correct percent
+    $incorrectCount = $track['incorrectCount'];
+    $correctPercent = $correctCount / ($correctCount + $incorrectCount) * 100;
+
+    // echo the average time and correct percent
+    echo 'Total Time: ' . $correctTime . '<br>';
+    echo "Average Time: " . $averageTime . '<br>';
+    echo "Percent Correct: " . $correctPercent . '<br>';
+    echo '<br>';
+
+    // update the online database
+    $sql = "UPDATE itunes_tracks SET averageTime = :averageTime, correctPercent = :correctPercent WHERE id = :id";
+    $sth = $dbh_online->prepare($sql);
+    $sth->execute(
+        array(
+            ":id" => $track['id'],
+            ":averageTime" => $averageTime,
+            ":correctPercent" => $correctPercent
+        )
+    );
+
+    // update the local database
+    $sql = "UPDATE itunes_tracks SET correctTime = :correctTime, correctCount = :correctCount, incorrectCount = :incorrectCount, averageTime = :averageTime, correctPercent = :correctPercent WHERE id = :id";
+    $sth = $dbh->prepare($sql);
+    $sth->execute(
+        array(
+            ":id" => $track['id'],
+            ":correctTime" => $correctTime,
+            ":correctCount" => $correctCount,
+            ":incorrectCount" => $incorrectCount,
+            ":averageTime" => $averageTime,
+            ":correctPercent" => $correctPercent
+        )
+    );
+}
+
+
+?>
